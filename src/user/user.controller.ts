49cf0acefa4f937b93734b1a19757de7d33678cb
Igor <igorpcampos2004@gmail.com>
2024-07-11 13:23:48 -0300

    feat: Criei conexão com o banco de dados

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,61 +3,54 @@ import {
     Controller,
     Delete,
     Get,
-    Param,
-    ParseIntPipe,
     Patch,
     Post,
     Put,
+    UseInterceptors,
 } from "@nestjs/common";
 import { CreateUserDTO } from "./dto/create-user.dto";
 import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
 import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
+import { UserService } from "./user.service";
+import { LogInterceptor } from "src/interceptors/log.interceptor";
+import { ParamId } from "src/decorators/param-id.decorator";
 
+@UseInterceptors(LogInterceptor) //Coloca o interceptor em todas as rotas
 @Controller("users")
 export class UserController {
+    constructor(private readonly userService: UserService) {}
+
     @Get()
-    async read() {
-        return { users: [] };
+    async list() {
+        return this.userService.list();
     }
 
     @Get(":id")
-    async readOne(@Param("id", ParseIntPipe) id: number) {
-        return { user: [], id };
+    async listOne(@ParamId() id: number) {
+        return this.userService.listOne(id);
     }
 
+    @UseInterceptors(LogInterceptor) //Coloca o interceptor somente nessa rota
     @Post()
-    async create(@Body() { email, name, password }: CreateUserDTO) {
-        return { email, name, password };
+    async create(@Body() data: CreateUserDTO) {
+        return this.userService.create(data);
     }
 
     @Patch(":id")
     async updatePartial(
-        @Body() { email, name, password }: UpdatePatchUserDTO,
-        @Param("id", ParseIntPipe) id: number,
+        @Body() data: UpdatePatchUserDTO,
+        @ParamId() id: number,
     ) {
-        return {
-            email,
-            name,
-            password,
-            id,
-        };
+        return this.userService.updatePartial(id, data);
     }
 
     @Put(":id")
-    async update(
-        @Body() { email, name, password }: UpdatePutUserDTO,
-        @Param("id", ParseIntPipe) id: number,
-    ) {
-        return {
-            email,
-            name,
-            password,
-            id,
-        };
+    async update(@Body() data: UpdatePutUserDTO, @ParamId() id: number) {
+        return this.userService.update(id, data);
     }
 
     @Delete(":id")
-    async delete(@Param("id", ParseIntPipe) id: number) {
-        return { id };
+    async delete(@ParamId() id: number) {
+        return this.userService.delete(id);
     }
 }
